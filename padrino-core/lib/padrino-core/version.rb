8f541eb616b10e514101dd8236ffc342c290033e
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-10-22 02:15:39 +0200

    Version done properly

--- padrino-core/lib/padrino-core/version.rb
+++ padrino-core/lib/padrino-core/version.rb
@@ -1,15 +1,23 @@
-##
-# Manages current Padrino version for use in gem generation.
-#
-# We put this in a separate file so you can get padrino version
-# without include full padrino core.
-#
 module Padrino
-  VERSION = '0.9.18' unless defined?(Padrino::VERSION)
+  ##
+  # Manages current Padrino version for use in gem generation.
+  #
+  # We put this in a separate file so you can get padrino version
+  # without include full padrino core.
+  #
+  module Version
+    unless defined?(Padrino::VERSION)
+      MAJOR  = 1
+      MINOR  = 0
+      PATH   = 0
+      STRING = [MAJOR, MINOR, PATCH].join('.')
+    end
+  end
+  
   ##
   # Return the current Padrino version
   #
   def self.version
-    VERSION
+    Version::STRING
   end
 end # Padrino
