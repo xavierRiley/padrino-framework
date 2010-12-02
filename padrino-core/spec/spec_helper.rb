606a74f4ef43c15f5c4388ed21d168e6b4648794
Chris Kowalik <chris@nu7hat.ch>
2010-12-03 00:43:32 +0100

    Common rspec config + fixed padrino cache rakefile

--- padrino-core/spec/spec_helper.rb
+++ padrino-core/spec/spec_helper.rb
@@ -1,15 +1,2 @@
 require File.expand_path('../../../load_paths', __FILE__)
-
-require 'mocha'
-require 'rspec'
-require 'padrino'
-
-RSpec.configure do |conf|
-  conf.include Padrino::Testing
-  conf.mock_with :mocha
-  conf.exclusion_filter = { 
-    :ruby => lambda { |version| 
-      !(RUBY_VERSION.to_s =~ /^#{version.to_s}/) 
-    }
-  }
-end
+require File.expand_path('../../../rspec_config', __FILE__)
--- padrino/spec/spec_helper.rb
+++ padrino/spec/spec_helper.rb
@@ -1,9 +1,2 @@
 require File.expand_path('../../../load_paths', __FILE__)
-
-require 'mocha'
-require 'rspec'
-require 'rack/test'
-
-RSpec.configure do |conf|
-  conf.mock_with :mocha
-end
+require File.expand_path('../../../rspec_config', __FILE__)
